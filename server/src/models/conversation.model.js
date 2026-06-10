2a09e0548af54fc5c6aef4e94841b59471b3858d
ionfwsrijan <ionfwsrijan@users.noreply.github.com>
2026-06-10 20:02:47 +0530

    fix: delete orphaned message notifications on conversation hard-delete (#753)
    - Add Notification.deleteMany in deleteConversation allDeleted branch
    - Add defensive null check in NotificationPanel.tsx for message notifications referencing deleted conversations
    - Show 'Conversation deleted' fallback text for orphaned message notifications

--- server/src/models/conversation.model.js
+++ server/src/models/conversation.model.js
@@ -24,15 +24,4 @@ const conversationSchema = new mongoose.Schema(
 
 conversationSchema.index({ participantsKey: 1 }, { unique: true, sparse: true });
 
-conversationSchema.pre("deleteOne", { document: false, query: true }, function (next) {
-  const convoId = this.getFilter()._id;
-  if (convoId) {
-    mongoose.model("Notification").deleteMany({ conversation: convoId, type: "message" })
-      .then(() => next())
-      .catch(next);
-  } else {
-    next();
-  }
-});
-
 export default mongoose.model("Conversation", conversationSchema);
